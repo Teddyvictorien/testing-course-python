a56d6d09d16200e12fb03eb0e380d7e7050c8f1a
Teddy Victorien <teddyvictorien@gmail.com>
2023-01-12 18:22:44 +0100

    services tests

--- tests/integration/say_hello_route_test.py
+++ tests/integration/say_hello_route_test.py
@@ -3,8 +3,9 @@ from app import app
 
 
 def test_say_hello_route_return_say_property():
-    response = app.test_client().get('/hello')
+    response = app.test_client().get('/character/1/movies')
     data = json.loads(response.data.decode('utf-8'))
+    print(data)
 
-    assert data['say'] == "hello", "it does not say hello"
+    assert data['movies'] == [{'title': 'A New Hope'}, {'title': 'The Empire Strikes Back'}, {'title': 'Return of the Jedi'}, {'title': 'Revenge of the Sith'}, {'title': 'The Force Awakens'}]
     assert response.status_code == 200
